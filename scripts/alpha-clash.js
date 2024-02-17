5883d365e4477d176f492258d0fa4a15f966653a
jahidcx <muhdjahidhasan@gmail.com>
2024-02-17 19:42:36 +0600

    created random alphabet

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,7 +10,21 @@
 
 //  } 
 
+function continueGame (){
+   const alphabet = getARandomAlphabet();
+   console.log('Your alphabet:', alphabet);
+
+   // set randomly generated  alphabet on screen 
+   const currentAlphabet = document.getElementById('current-alphabet');
+   currentAlphabet.innerText = alphabet; 
+
+   // set bg color 
+   setBGColorById(alphabet); 
+
+}
+
 function play(){
    hideElementById('home-screen');
    showElementById('play-section');
+   continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -8,5 +8,27 @@ function hideElementById(elementId){
 function showElementById (elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
-    
+
+}
+
+// set bg color to selected random alphabet
+function setBGColorById (elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-300');
+}
+
+function getARandomAlphabet (){
+    // get or create random alphabet array
+    const alphabetString = 'asdylheiosdnvfjesdhnvbxmzwopuinqyiombvuiafrh';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+
+    // get a random index between o to 43
+    const randomNumber = Math.random()*43;
+    const index = Math.round(randomNumber);
+   
+    const alphabet = alphabets[index];
+
+    // console.log(index,alphabet);
+    return alphabet;
 }
