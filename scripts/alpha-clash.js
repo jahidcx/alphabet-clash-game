94efdd24f34e3ebac43045e18631d83e183eae83
jahidcx <muhdjahidhasan@gmail.com>
2024-02-18 03:32:17 +0600

    get score and manage life

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,6 +10,76 @@
 
 //  } 
 
+function handleKeyBoardButtonPress (event){
+   const playerPressed = event.key;
+   console.log(playerPressed );
+
+   // get the expected to press
+   const currentAlphabetElement = document.getElementById('current-alphabet');
+   const currentAlphabet = currentAlphabetElement.innerText; 
+   const expectedAlphabet = currentAlphabet.toLowerCase();
+   console.log(playerPressed, expectedAlphabet);
+
+
+   // check match or not
+   if(playerPressed === expectedAlphabet){
+      console.log('You get a point');
+
+      const currentScore = getTextElementById('current-score');
+      const updatedScore = currentScore +1;
+      setUpdateScoreAndLifeById  ('current-score', updatedScore);
+
+
+
+
+      // update score
+      // // 1. get the current score
+      // const currentScoreElement = document.getElementById('current-score');
+      // const currentScoreText = currentScoreElement.innerText;
+      // const currentScore = parseInt(currentScoreText);
+
+      // // increase by 1
+      // const newScore = currentScore + 1;
+      // console.log(newScore);
+
+      // // display new score
+      // currentScoreElement.innerText = newScore ;  
+
+      // play agian
+      removeBGColorById(expectedAlphabet);
+      continueGame ();
+   }
+   else{
+      console.log('You lost a life.');
+
+      const currentLife = getTextElementById('current-life');
+      const updatedLife = currentLife - 1;
+      setUpdateScoreAndLifeById ('current-life', updatedLife);
+
+
+
+
+
+
+      // update life
+      // 1.get the current life
+      // const currentLifeElement = document.getElementById('current-life');
+      // const currentLifeText = currentLifeElement.innerText;
+      // const currentLife = parseInt(currentLifeText);
+
+      // // decrease by 1
+      // const newLife = currentLife - 1;
+
+      // // display new life 
+      // currentLifeElement.innerText = newLife;  
+   }
+
+}
+// capture keyboard key press
+document.addEventListener('keyup',handleKeyBoardButtonPress )
+
+
+
 function continueGame (){
    const alphabet = getARandomAlphabet();
    console.log('Your alphabet:', alphabet);
--- scripts/utility.js
+++ scripts/utility.js
@@ -16,15 +16,33 @@ function setBGColorById (elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-300');
 }
+// remove bg color 
+function removeBGColorById (elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-300');
+}
+
+// score update 
+function getTextElementById (elementId){
+    const element = document.getElementById(elementId);
+   const elementText = element.innerText;
+   const value = parseInt(elementText);
+   return value; 
+}
+// set update score
+function setUpdateScoreAndLifeById (elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value; 
+}
 
 function getARandomAlphabet (){
     // get or create random alphabet array
-    const alphabetString = 'asdylheiosdnvfjesdhnvbxmzwopuinqyiombvuiafrh';
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
     // console.log(alphabets);
 
     // get a random index between o to 43
-    const randomNumber = Math.random()*43;
+    const randomNumber = Math.random()*25;
     const index = Math.round(randomNumber);
    
     const alphabet = alphabets[index];
