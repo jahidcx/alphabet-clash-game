96eacff218b05a18747eaf922aaea16294ea2a47
jahidcx <muhdjahidhasan@gmail.com>
2024-02-17 17:46:02 +0600

    hide home screen and show play section

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,4 +1,16 @@
  
- function playNow (){
-    console.log('play Now')
- }
+//  function playNow (){
+//    // hide home section
+//    const homeSection = document.getElementById('home-screen');
+//    homeSection.classList.add('hidden');
+
+//    // show the play section
+//    const playSection = document.getElementById('play-section');
+//    playSection.classList.remove('hidden');
+
+//  } 
+
+function play(){
+   hideElementById('home-screen');
+   showElementById('play-section');
+}
